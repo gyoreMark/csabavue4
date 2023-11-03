0dd1dc6c8f69d264fdf7ac0c0350cffadf33e427
Györe Márk <gyore.mark@students.jedlik.eu>
2023-11-03 15:27:31 +0100

    nm

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,85 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="background">
+    <div class="container">
+      <h1 class="text-light">KÖR</h1>
+      <div class="input-group mb-3">
+        <input
+          type="number"
+          class="form-control"
+          aria-label="Recipient's username"
+          aria-describedby="button-addon2"
+          id="sugarInput"
+          placeholder="Kör sugara (r)"
+          min="1"
+          v-model="sugarInput"
+        />
+        <button
+          class="btn btn-outline-light"
+          type="button"
+          id="addBtn"
+          @click="szamitas"
+        >
+          Számolás
+        </button>
+      </div>
+
+      <div class="row">
+        <div class="col">
+          Terület
+          <div class="alert alert-light" role="alert" id="">
+            {{ teruletSzamitas }}
+          </div>
+        </div>
+        <div class="col">
+          Kerület
+          <div class="alert alert-light" role="alert" id="">
+            {{ keruletSzamitas }}
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: "HelloWorld",
   props: {
-    msg: String
-  }
-}
+    msg: String,
+  },
+
+  data() {
+    return {
+      sugarInput: undefined,
+      teruletSzamitas: undefined,
+      keruletSzamitas: undefined,
+    };
+  },
+
+  methods: {
+    szamitas: function () {
+      //terulet
+      this.teruletSzamitas = Math.PI * Math.pow(this.sugarInput, 2);
+
+      //kerulet
+      this.keruletSzamitas = 2 * Math.PI * this.sugarInput;
+    },
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+.background {
+  position: absolute;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  width: 100%;
+  height: 100%;
+  background-color: rgb(90, 61, 61);
+  top: 0;
+  left: 0;
+  color: white;
 }
 </style>
